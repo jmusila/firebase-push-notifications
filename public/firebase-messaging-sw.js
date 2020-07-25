382efccbeb7cdaa629a61e01d5cbb48f8fcc8aaf
jonathanmusila <jonathanmusila6@gmail.com>
2020-07-25 11:54:12 +0300

    edit js

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -10,13 +10,13 @@ Initialize the Firebase app in the service worker by passing in the messagingSen
 * New configuration for app@pulseservice.com
 */
 firebase.initializeApp({
-    apiKey: '{{env('FIREBASE_API_KEY')}}',
-    authDomain: '{{env('FIREBASE_AUTH_DOMAIN')}}',
-    databaseURL:'{{env('FIREBASE_DATABASE_URL')}}',
-    projectId: '{{env('FIREBASE_PROJECT_ID')}}',
-    storageBucket: '{{env('FIREBASE_STORAGE_BUCKET')}}',
-    messagingSenderId: '{{env('FIREBASE_MESSAGING_SENDER_ID')}}',
-    appId: '{{env('FIREBASE_APP_ID')}}',
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL:process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
 });
 
 /*
